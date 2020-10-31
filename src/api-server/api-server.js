c2448eb12e0bf5012f174d77f12fac8f98e9da42
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-31 12:15:21 +0100

    add api server to get the phones information

---
+++ src/api-server/api-server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const fs = require('fs');
+
+let phonesFromApi = JSON.parse(fs.readFileSync('./phones.json'));
+
+app.get('/phones', (req, res) => {
+  res.send(phonesFromApi);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/phones`);
+});
