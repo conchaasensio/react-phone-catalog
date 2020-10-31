414e0405472c4a365bb1e87c9467ff9ca6e4c739
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-31 11:20:23 +0100

    import data from Api to save it as initial state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
+import phonesFromApi from './data/phones.json';
 import './App.css';
 
-function App() {
+const App = () => {
   return <div className="App">Hola, mundo</div>;
-}
+};
 
 export default App;
