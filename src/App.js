20626136ac0a2d2aa25c9d54950b12c2fd260831
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-31 11:35:56 +0100

    render phone cagtalog

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ const App = () => {
   const [phones] = useState(phonesFromApi);
   console.log(phones);
 
+  const phoneList = phones.map((phone, index) => {
+    return (
+      <li key={index}>
+        <h2>{phone.name}</h2>
+        <img src={phone.imageFileName} alt={phone.name} />
+      </li>
+    );
+  });
+
   return (
     <div className="App">
       <h1>Catálogo de teléfonos</h1>
+      <ul>{phoneList}</ul>
     </div>
   );
 };
