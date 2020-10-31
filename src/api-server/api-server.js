e95a8ac7f732a336415a8998c4b95b72ebb35631
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-31 12:46:01 +0100

    add cors middleware

--- src/api-server/api-server.js
+++ src/api-server/api-server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 4000;
 const fs = require('fs');
+var cors = require('cors');
+
+app.use(cors());
 
 let phonesFromApi = JSON.parse(fs.readFileSync('./phones.json'));
 
