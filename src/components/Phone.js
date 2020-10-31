5cc761bb43cbee9920cf02730bb00237f614d414
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-31 11:54:54 +0100

    render phone prices

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -6,6 +6,7 @@ const Phone = (props) => {
       <article className="phone">
         <div className="phone__container">
           <h2 className="phone__title">{props.name}</h2>
+          <p className="phone__price">{props.price}€</p>
         </div>
         <img className="phone__image" src={props.image} alt={props.name} />
       </article>
