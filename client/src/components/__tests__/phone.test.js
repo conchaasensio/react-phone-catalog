3371af52bfc6922a4f1251fde6237027ef1cbab2
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-06 19:28:15 +0100

    refactor hide irrelevant data from the test

--- client/src/components/__tests__/phone.test.js
+++ client/src/components/__tests__/phone.test.js
@@ -4,20 +4,23 @@ import Phone from '../Phone';
 import { MemoryRouter } from 'react-router-dom';
 
 test('creates a link to phone detail using its id', async () => {
-  const id = 123;
+  const props = validProps();
+  props.id = 123;
 
-  const { getByRole } = render(
-    <Phone
-      id={id}
-      name="iPhone 7"
-      price="600"
-      image="http://www.host.com/image.jpg"
-    />
-  );
+  const { getByRole } = render(<Phone {...props} />);
 
   const link = getByRole('link');
-  expect(link.href).toBe(`http://localhost/phone/${id}`);
+  expect(link.href).toBe(`http://localhost/phone/123`);
 });
+
+function validProps() {
+  return {
+    id: 1,
+    name: 'iPhone 7',
+    price: '600',
+    image: 'htpp://www.host.com/image.jpg',
+  };
+}
 
 function render(component) {
   // https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/testing.md
