feb7e8b3478840877ba9998958be78bc1872b604
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-06 19:21:49 +0100

    refactor test in Phone hiding MemoryRouter from the test

--- client/src/components/__tests__/phone.test.js
+++ client/src/components/__tests__/phone.test.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render as reactRender } from '@testing-library/react';
 import Phone from '../Phone';
 import { MemoryRouter } from 'react-router-dom';
 
-// https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/testing.md
 test('creates a link to phone detail using its id', async () => {
   const id = 123;
 
   const { getByRole } = render(
-    <MemoryRouter>
-      <Phone
-        id={id}
-        name="iPhone 7"
-        price="600"
-        image="http://www.host.com/image.jpg"
-      />
-    </MemoryRouter>
+    <Phone
+      id={id}
+      name="iPhone 7"
+      price="600"
+      image="http://www.host.com/image.jpg"
+    />
   );
 
   const link = getByRole('link');
   expect(link.href).toBe(`http://localhost/phone/${id}`);
 });
+
+function render(component) {
+  // https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/testing.md
+  return reactRender(<MemoryRouter>{component}</MemoryRouter>);
+}
