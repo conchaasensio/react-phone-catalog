7bfc3e3939c5e905e78e333b6e359192d1025109
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-08 19:52:15 +0100

    use environment variable for API url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,5 @@
 const getDataFromAPi = () => {
-  const url = process.env.PORT
-    ? 'https://murmuring-woodland-48882.herokuapp.com/phones'
-    : 'http://localhost:4000/phones';
+  const url = process.env.API_URL || 'http://localhost:4000/phones';
   return fetch(url).then((response) => response.json());
 };
 
