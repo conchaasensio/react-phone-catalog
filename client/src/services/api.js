9ddf7c9a5d9f51a5f688fa7d64e0ffc915cd02ea
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-08 20:11:40 +0100

    using REACT_APP as prefix for environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 const getDataFromAPi = () => {
-  const url = process.env.API_URL || 'http://localhost:4000/phones';
+  const url = process.env.REACT_APP_API_URL || 'http://localhost:4000/phones';
   return fetch(url).then((response) => response.json());
 };
 
