af7b1450f98278c9cea6c3eace502312a55f7715
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-08 19:45:02 +0100

    configure client to be deployed in heroku

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 const getDataFromAPi = () => {
-  return fetch('http://localhost:4000/phones').then((response) =>
-    response.json()
-  );
+  const url = process.env.PORT
+    ? 'https://murmuring-woodland-48882.herokuapp.com/phones'
+    : 'http://localhost:4000/phones';
+  return fetch(url).then((response) => response.json());
 };
 
 export default getDataFromAPi;
